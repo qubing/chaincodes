7f76329259331a42a17247699ebe49b76e5729a4
qubing <23227732@qq.com>
2018-11-20 18:03:59 +0800

    crypto_ledger added.

--- go/build.sh
+++ go/build.sh
@@ -1,8 +1,19 @@
 mkdir $GOPATH/src/github.com
-rm $GOPATH/src/github.com/chaincode
+rm $GOPATH/src/github.com/chaincodes
 ln -s 
-ln -s $PWD $GOPATH/src/github.com/chaincode
-cd $GOPATH/src/github.com/chaincode/balance_mgr
+ln -s $PWD $GOPATH/src/github.com/chaincodes
+
+#balance_mgr
+cd $GOPATH/src/github.com/chaincodes/balance_mgr
+rm -rf vendor
+govendor init
+govendor add +external
+govendor add github.com/hyperledger/fabric/peer
+govendor add github.com/hyperledger/fabric/core/chaincode/lib/cid
+go build
+
+#crypto_ledger
+cd $GOPATH/src/github.com/chaincodes/crypto_ledger
 rm -rf vendor
 govendor init
 govendor add +external
